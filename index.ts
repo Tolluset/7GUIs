0ff869e1d87d55b10f042dd56cf404ab980b8d45
tolluset <dlqud19@gmail.com>
2024-01-02 19:21:58 +0900

    feat: add temperature converter

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import { TemperatureConverter } from "./src/temperature-converter/temperature-converter.js";
 import { Counter } from "./src/counter/counter.js";
 
 const main = () => {
@@ -8,6 +9,7 @@ const main = () => {
   }
 
   new Counter(app);
+  new TemperatureConverter(app);
 };
 
 main();
