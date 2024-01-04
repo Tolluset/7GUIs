3cd39de059b10c53df33ea5277f68b5112476f46
tolluset <dlqud19@gmail.com>
2024-01-04 17:29:15 +0900

    refactor: adjust to base component

--- src/counter/counter.ts
+++ src/counter/counter.ts
@@ -1,50 +1,46 @@
-export class Counter {
-  private element: Element;
-  private layout: Element;
+import { Base } from "../lib/base.js";
 
-  private count: number;
+interface State {
+  count: number;
+}
+
+const state: State = {
+  count: 0,
+};
+
+export class Counter extends Base<State> {
+  protected layout: Element;
 
   constructor(element: Element) {
-    this.element = element;
-    this.count = 0;
-
-    this.layout = document.createElement("div");
-    this.element.appendChild(this.layout);
-
-    this.render();
+    super(element, state);
   }
 
   private increase() {
-    this.count++;
-
-    this.render();
+    this.state.count++;
   }
 
-  private template() {
+  protected template() {
     const _template = document.createElement("template");
 
     _template.innerHTML = `
       <div class="counter-wrapper">
         <div class="counter-title">Counter</div>
         <div class="counter-contents">
-          <input id="counter" readOnly="true" value=${this.count} class="counter-input"></input>
+          <input id="counter" readOnly="true" value=${this.state.count} class="counter-input"></input>
           <button type="button" class="counter-button">Count</button>
         </div>
       </div>
   `;
 
-    return _template;
+    return _template.content;
   }
 
-  private render() {
-    const template = this.template().content;
+  protected events() {
+    const t = this.template();
 
-    const input = template.querySelector<HTMLInputElement>(".counter-input");
-    input.value = this.count.toString();
-
-    const button = template.querySelector<HTMLButtonElement>(".counter-button");
+    const button = t.querySelector<HTMLButtonElement>(".counter-button");
     button.addEventListener("click", this.increase.bind(this));
 
-    this.layout.replaceChildren(template);
+    return t;
   }
 }
